cbd7420e05d7f4b56e15bfd1a3b0546a25eeec6e
AlexBnd11 <alexbonniard@gmail.com>
2024-10-23 09:08:38 +0200

    commencement page Logement

--- src/pages/Logement.js
+++ src/pages/Logement.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import Collapse from "../components/Collapse"
+import ArrowForward from "../assets/arrow-forward.png";
 
-export default function Logement() {
+export default function Logement(props) {
+    const [currentImage, setCurrentImage] = useState([props.pictures]);
+    const prevHandler = () => {
+        if (currentImage[0]) {
+            setCurrentImage[currentImage.length -1]
+        } else {
+            setCurrentImage[currentImage - 1]
+        }
+    };
+    const nextHandler = () => {
+        if (currentImage = currentImage.length -1) {
+            setCurrentImage[0]
+        } else {
+            setCurrentImage[currentImage + 1]
+        }
+    };
+
     return (
-        <h1>Hello World</h1>
+        <main>
+
+            <div className="carrousel-container">
+                <figure>{currentImage}</figure>
+                <img src={ArrowForward} className="arrow-backwards" onClick={prevHandler}/>
+                <img src={ArrowForward} className="arrow-forward" onClick={nextHandler}/>
+            </div>
+            <div className="description--primary">
+                <div className="title-container">
+                    <h1>{props.title}</h1>
+                    <h2>{props.location}</h2>
+                </div>
+                <div className="host-container">
+                    <p>{props.hostName}</p>
+                    <img src={props.hostPic} />
+                </div>
+            </div>
+            <div className="description--secondary">
+                <div className="tags-container">
+                    <p>{props.tags}</p>
+                </div>
+                <div className="stars-container">
+                    <img />
+                    <img />
+                    <img />
+                    <img />
+                    <img />
+                </div>
+            </div>
+            <div className="description--tertiary">
+                <Collapse 
+                    collapseName="Description"
+                    collapseDescription={props.description}
+                    />
+                <Collapse 
+                    collapseName="Equipements"
+                    collapseDescription={props.equipements}
+                    />
+            </div>
+        </main>
     );
 }
